9f3533024363f3888f98e3a7acf9be42929750a0
Tom Needham <06needhamt@gmail.com>
2021-03-13 16:47:19 +0000

    avformat: Add Dynacolor MVC Demuxer
    This Patch adds a demuxer for the Dynacolor file format based on a sample file located at.
    
    https://streams.videolan.org/samples/camera-dvr/dynacolor/dynacolor-camera-sample

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -140,6 +140,7 @@ extern AVOutputFormat ff_dv_muxer;
 extern AVInputFormat  ff_dvbsub_demuxer;
 extern AVInputFormat  ff_dvbtxt_demuxer;
 extern AVInputFormat  ff_dxa_demuxer;
+extern AVInputFormat  ff_dynacolor_demuxer;
 extern AVInputFormat  ff_ea_demuxer;
 extern AVInputFormat  ff_ea_cdata_demuxer;
 extern AVInputFormat  ff_eac3_demuxer;
